ac8d707add20a574282276c336135d9b08e6a48f
Artyom Pogosov <artyomp@outlook.com>
2019-10-15 21:07:16 +0300

    my new commit

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function createProjects() {
     return [
         createProj('Mine-sweeper', 'MineSweeper', 'MineSweeper game', 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.', "proj/MineSweeper", ' 09/2019', 'aaaaaaa', 'Mine-Sweeper'),
         createProj('Blogin-website', 'Blogin', 'lorem ipsum lorem', 'lorem ipsum lorem ipsum lorem ipsum', "proj/blogin-site", 'aaaa', 'aaaaaaa', 'blogin-site'),
-        createProj('Touchnums', 'Touch-nums', 'Touch the Numbers game', 'Touch Nums is an addicting, fun game that will test your reaction time and see how fast your fingers are.', "proj/touch-nums", 'aaaa', 'aaaaaaa'),
-        createProj('GussWho', 'Guss-Who', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', "proj/touch-nums", 'aaaa', 'aaaaaaa'),
+        createProj('Touchnums', 'Touch-nums', 'Touch the Numbers game', 'Touch Nums is an addicting, fun game that will test your reaction time and see how fast your fingers are.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'Touch-the-numbers'),
+        createProj('GussWho', 'Guss-Who', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'GuessWho'),
     ];
 }
 
