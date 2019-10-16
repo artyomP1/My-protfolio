be58eb03db0d4b02c9ea555e4323753342177e10
Artyom Pogosov <artyomp@outlook.com>
2019-10-16 15:50:08 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ gProjects = createProjects();
 
 function createProjects() {
     return [
-        createProj('Mine-sweeper', 'MineSweeper', 'MineSweeper game', 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.', "proj/MineSweeper", ' 09/2019', 'aaaaaaa', 'Mine-Sweeper'),
-        createProj('Blogin-website', 'Blogin', 'lorem ipsum lorem', 'lorem ipsum lorem ipsum lorem ipsum', "proj/blogin-site", 'aaaa', 'aaaaaaa', 'blogin-site'),
-        createProj('Touchnums', 'Touch-nums', 'Touch the Numbers game', 'Touch Nums is an addicting, fun game that will test your reaction time and see how fast your fingers are.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'Touch-the-numbers'),
-        createProj('GussWho', 'Guss-Who', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'GuessWho'),
-        createProj('BookShop', 'Book-shop', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'Book-shop'),
-        createProj('CollectThoseBalls', 'Collect-those-Balls', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', "proj/touch-nums", 'aaaa', 'aaaaaaa', 'Ball-board'),
+        createProj('Mine-sweeper', 'MineSweeper', 'MineSweeper game', 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.', "https://artyomp1.github.io/Mine-Sweeper/", ' 09/2019', 'aaaaaaa'),
+        createProj('Blogin-website', 'Blogin', 'lorem ipsum lorem', 'lorem ipsum lorem ipsum lorem ipsum', 'https: //artyomp1.github.io/blogin-site/', 'aaaa', 'aaaaaaa', 'blogin-site'),
+        createProj('Touchnums', 'Touch-nums', 'Touch the Numbers game', 'Touch Nums is an addicting, fun game that will test your reaction time and see how fast your fingers are.', "https://artyomp1.github.io/Touch-the-numbers/", 'aaaa', 'aaaaaaa'),
+        createProj('GussWho', 'Guss-Who', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', 'https://artyomp1.github.io/GuessWho/', 'aaaa', 'aaaaaaa'),
+        createProj('BookShop', 'Book-shop', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', 'https://artyomp1.github.io/Book-shop/', 'aaaa', 'aaaaaaa'),
+        createProj('CollectThoseBalls', 'Collect-those-Balls', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', 'https://artyomp1.github.io/Ball-board/', 'aaaa', 'aaaaaaa'),
     ];
 }
 
@@ -18,7 +18,7 @@ function projects() {
     return gProjects;
 }
 
-function createProj(id, name, title, desc, url, publishedAt, labels, githube) {
+function createProj(id, name, title, desc, url, publishedAt, labels) {
     return {
         id: id,
         name: name,
@@ -27,7 +27,6 @@ function createProj(id, name, title, desc, url, publishedAt, labels, githube) {
         url: url,
         publishedAt: publishedAt,
         labels: labels,
-        githube: githube,
     }
 }
 
--- js/offCanvas.js
+++ js/offCanvas.js
@@ -1,4 +1,6 @@
 function openCanvas() {
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
+
+    // document.querySelector('.offcanvas-btn').innerHTML = 'X';
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');
 }
