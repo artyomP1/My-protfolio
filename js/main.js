67b1d92767777d9704bb0c1e3ad051d21038b0e6
Artyom Pogosov <artyomp@outlook.com>
2019-10-18 11:53:01 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ gProjects = createProjects();
 function createProjects() {
     return [
         createProj('Mine-sweeper', 'MineSweeper', 'MineSweeper game', 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.', "https://artyomp1.github.io/Mine-Sweeper/", ' 09/2019', 'aaaaaaa'),
-        createProj('Blogin-website', 'Blogin', 'lorem ipsum lorem', 'lorem ipsum lorem ipsum lorem ipsum', 'https: //artyomp1.github.io/blogin-site/', 'aaaa', 'aaaaaaa'),
+        createProj('Blogin-website', 'Blogin', 'lorem ipsum lorem', 'lorem ipsum lorem ipsum lorem ipsum', 'https://artyomp1.github.io/blogin-site/', 'aaaa', 'aaaaaaa'),
         createProj('Touchnums', 'Touch-nums', 'Touch the Numbers game', 'Touch Nums is an addicting, fun game that will test your reaction time and see how fast your fingers are.', "https://artyomp1.github.io/Touch-the-numbers/", 'aaaa', 'aaaaaaa'),
         createProj('GussWho', 'Guss-Who', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', 'https://artyomp1.github.io/GuessWho/', 'aaaa', 'aaaaaaa'),
         createProj('BookShop', 'Book-shop', 'lorem ipsum loremv', 'Tlorem ipsum lorem, florem ipsum lorem.', 'https://artyomp1.github.io/Book-shop/', 'aaaa', 'aaaaaaa'),
